9261679736eb907e37ffc8d703c60a7a494aba1b
kumarianamika11059-sketch <kumarianamika11059@gmail.com>
2026-09-04 20:40:24 +0530

    L1 done

---
+++ Student.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Student{
+    String name;
+    int marks;
+    }
+    public void main(){
+        Student stud=new Student();
+        stud.name="Divya";
+        stud.marks=88;
+       System.out.print("Name : "+stud.name+" | "+" Marks :"+stud.marks);
+        
+    }
