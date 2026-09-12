52ede01cf025fb6887598f523a8da069fe11e142
kumarianamika11059-sketch <kumarianamika11059@gmail.com>
2026-09-12 19:43:35 +0530

    week4_assignment done

---
+++ ExceptSelf.java
@@ -0,0 +1,23 @@
+public class ExceptSelf {
+ static int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        int product=1;
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = product;
+            product = product * nums[i];
+        }
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * product;
+            product = product * nums[i];
+        }
+             return answer;
+    }
+    public static void main(String[] args) {
+      int[] nums = {1, 2, 3, 4};
+      int[] result = productExceptSelf(nums);
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
